638161fcc5f51cf6506aa3df298137f0e989e2c8
Kevin Fritz <Kevin.F1@gmx.net>
2017-06-29 14:43:50 +0200

    added Pong_Test - läuft version 1

--- Image_Generator.vhd
+++ Image_Generator.vhd
@@ -52,7 +52,7 @@ begin
 					elsif (VGA_Spielfeld_Pixel_En) then
 						VGA_Pixel_Color <= VGA_Spielfeld_Pixel_Color;
 					else
-						VGA_Pixel_Color <= x"A"; --Background Color
+						VGA_Pixel_Color <= x"2"; --Background Color
 					end if;
 					
 				else  --setze alles auf Schwarz, wenn kein Enable vorhanden ist
@@ -115,7 +115,24 @@ begin
 											VGA_B <= x"FF"; 
 						when others => VGA_R <= (others => '0');
 											VGA_G <= (others => '0');
-											VGA_B <= (others => '0'); 		 
+											VGA_B <= (others => '0');										
+						--CGA	RGB	Farbe
+						--0x0	0,0,0	black
+						--0x1	0,0,170	blue
+						--0x2	0,170,0	green
+						--0x3	0,170,170	cyan
+						--0x4	170,0,0	red
+						--0x5	170,0,170	magenta
+						--0x6	170,85,0	brown
+						--0x7	170,170,170	gray
+						--0x8	85,85,85	dark gray
+						--0x9	85,85,255	bright blue
+						--0xA	85,255,85	bright green
+						--0xB	85,255,255	bright cyan
+						--0xC	255,85,85	bright red
+						--0xD	255,85,255	bright magenta
+						--0xE	255,255,85	yellow
+						--0xF	255,255,255	white
 				end case;
 		end process;
 
---
+++ Pong_Test.vhd
@@ -0,0 +1,89 @@
+library ieee;
+
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity Pong_Test is
+
+port(
+	Reset: in std_logic;		--asynchroner Reset, High aktiv
+	Clk: in std_logic;		--50MHz Clock
+
+	-------------------------------------------------------------
+	VGA_HS: out std_logic;		--Horizontal Synchronimpuls, High aktiv
+	VGA_VS: out std_logic;		--Vertikal Synchronimpuls, High aktiv
+	-------------------------------------------------------------
+	VGA_R: out std_logic_vector(7 downto 0);		--Farbausgabe Rot aus LUT für DAC
+	VGA_G: out std_logic_vector(7 downto 0);		--Farbausgabe Grün aus LUT für DAC
+	VGA_B: out std_logic_vector(7 downto 0);		--Farbausgabe Blau aus LUT für DAC
+	VGA_Clk: out std_logic
+);
+
+end entity Pong_Test;
+
+architecture behave of Pong_Test is
+
+	-------------------------------------------------------------
+	signal VGA_Pos_X:  std_logic_vector(9 downto 0) := (others => '0');		--Horizontal X Koordinate --Standardwert neu hinzugefügt
+	signal VGA_Pos_Y:  std_logic_vector(9 downto 0) := (others => '0');		--Vertikal Y Koordinate  --Standardwert neu hinzugefügt
+	
+		-------------------------------------------------------------
+	--signal VGA_Paddle1_Pixel_En: in std_logic;		--Enable für aktuelles Pixel, High aktiv
+	--signal VGA_Paddle2_Pixel_En: in std_logic;		--Enable für aktuelles Pixel, High aktiv
+	--signal VGA_Ball_Pixel_En: in std_logic;			--Enable für aktuelles Pixel, High aktiv
+	signal VGA_Spielfeld_Pixel_En:  std_logic;	--Enable für aktuelles Pixel, High aktiv
+	-------------------------------------------------------------
+	--signal VGA_Paddle1_Pixel_Color: in std_logic_vector(3 downto 0);		--Farbe für aktuelles Pixel
+	--signal VGA_Paddle2_Pixel_Color: in std_logic_vector(3 downto 0);		--Farbe für aktuelles Pixel
+	--signal VGA_Ball_Pixel_Color: in std_logic_vector(3 downto 0);			--Farbe für aktuelles Pixel
+	signal VGA_Spielfeld_Pixel_Color:  std_logic_vector(3 downto 0);		--Farbe für aktuelles Pixel
+
+	begin
+	
+VGA: entity work.VGA
+
+		port map(Reset,
+					Clk,
+					--VGA_Paddle1_Pixel_En,
+					'0',
+					--VGA_Paddle2_Pixel_En,
+					'0',
+					--VGA_Ball_Pixel_En,
+					'0',
+					VGA_Spielfeld_Pixel_En,
+					--VGA_Paddle1_Pixel_Color,
+					"0000",
+					--VGA_Paddle2_Pixel_Color,
+					"0000",
+					--VGA_Ball_Pixel_Color,
+					"0000",
+					VGA_Spielfeld_Pixel_Color,
+					VGA_Pos_X,
+					VGA_Pos_Y,
+					VGA_HS,
+					VGA_VS,
+					VGA_R,
+					VGA_G,
+					VGA_B,
+					VGA_Clk);
+					
+Spielfeld: entity work.Spielfeld
+	port map(
+		--Goal1, 							
+		open,
+		--Goal2,							
+		open,
+		--Boarder_colission, 			
+		open,
+		VGA_Spielfeld_Pixel_Color, 	
+		VGA_Spielfeld_Pixel_En, 		
+		--Ball_Pos_X, 						
+		"0000000000",
+		--Ball_Pos_Y, 						
+		"0000000000",
+		VGA_Pos_X, 						
+		VGA_Pos_Y 			
+	);
+					
+					
+end architecture behave;
